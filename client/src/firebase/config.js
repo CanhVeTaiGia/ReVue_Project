634d78bc1f98a9bb5d511c4f97dbc9c67edf1806
CanhVeTaiGia <lvy16012005@gmail.com>
2024-11-10 16:21:56 +0700

    add

---
+++ client/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeTf5vZDqNM6gL_vxMZFb3mk2fkQlFZ8c",
+  authDomain: "data-cvtg.firebaseapp.com",
+  projectId: "data-cvtg",
+  storageBucket: "data-cvtg.appspot.com",
+  messagingSenderId: "202169001972",
+  appId: "1:202169001972:web:cfd543cad2f177e919bba1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
